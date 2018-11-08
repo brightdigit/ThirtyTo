b0685cb369a998045aa209d9dc2eb5f4621fe6a8
Leo Dion <leogdion@brightdigit.com>
2018-11-08 18:59:25 -0500

    fixing for linux

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/docopt/docopt.swift", .branch("master")),
+      .package(url: "https://github.com/elegantchaos/docopt.swift.git", from: "0.6.8"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
