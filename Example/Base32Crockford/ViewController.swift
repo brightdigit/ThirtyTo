d3a73a132311aac6ec00932b3b72c203cf2dc57b
Leo Dion <leogdion@brightdigit.com>
2018-11-07 20:52:24 -0500

    added spec code [skip ci]

---
+++ Example/Base32Crockford/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Base32Crockford
+//
+//  Created by leogdion@brightdigit.com on 11/07/2018.
+//  Copyright (c) 2018 leogdion@brightdigit.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Example/Pods/Target Support Files/Base32Crockford/Base32Crockford-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Base32Crockford : NSObject
+@end
+@implementation PodsDummy_Base32Crockford
+@end
