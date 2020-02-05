bd6a058877c22b8be67d0ed78de9baaf9539dbf8
Leo Dion <leogdion@brightdigit.com>
2020-02-05 14:28:51 -0500

    fixing linting issues

--- Tests/Base32CrockfordTests/ArrayTests.swift
+++ Tests/Base32CrockfordTests/ArrayTests.swift
@@ -2,9 +2,9 @@
 import XCTest
 
 final class ArrayTests: XCTestCase {
-  func testBadRandomArray () {
+  func testBadRandomArray() {
     var tested = false
-    let result = Array.debugRandom(withCount: -100, in: (0...100)) { (actual) in
+    let result = Array.debugRandom(withCount: -100, in: 0 ... 100) { actual in
       XCTAssertEqual(actual, "Array count cannot be less than 0.")
       tested = true
     }
