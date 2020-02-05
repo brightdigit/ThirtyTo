ad63d772828766cc0664289de3f0cea980d114df
Leo Dion <leogdion@brightdigit.com>
2020-02-05 14:22:32 -0500

    working unit tests for arrays

---
+++ Tests/Base32CrockfordTests/ArrayTests.swift
@@ -0,0 +1,14 @@
+@testable import Base32Crockford
+import XCTest
+
+final class ArrayTests: XCTestCase {
+  func testBadRandomArray () {
+    var tested = false
+    let result = Array.debugRandom(withCount: -100, in: (0...100)) { (actual) in
+      XCTAssertEqual(actual, "Array count cannot be less than 0.")
+      tested = true
+    }
+    XCTAssertTrue(tested)
+    XCTAssertNil(result)
+  }
+}
