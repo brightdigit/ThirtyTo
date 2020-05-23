38deb458e8f93488f3d5c99671cbe7e7a162accb
Leo Dion <leogdion@brightdigit.com>
2020-05-23 17:37:18 -0400

    added dynamic ubuntu and swift version

--- Scripts/script.sh
+++ Scripts/script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-SWIFT_VER="5.1.3"
-
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
   swiftformat --lint . && swiftlint
 elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
@@ -12,13 +10,13 @@ elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
 fi
 
 swift build
-swift test  --enable-code-coverage
+swift test  --enable-code-coverage --enable-test-discovery
 
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
   xcrun llvm-cov export -format="lcov" .build/debug/${FRAMEWORK_NAME}PackageTests.xctest/Contents/MacOS/${FRAMEWORK_NAME}PackageTests -instr-profile .build/debug/codecov/default.profdata > info.lcov
   bash <(curl https://codecov.io/bash) -F travis -F macOS -n $TRAVIS_JOB_NUMBER-$TRAVIS_OS_NAME
 else
-  llvm-cov export -format="lcov" .build/x86_64-unknown-linux/debug/${FRAMEWORK_NAME}PackageTests.xctest -instr-profile .build/debug/codecov/default.profdata > info.lcov
+  llvm-cov export -format="lcov" .build/x86_64-unknown-linux-gnu/debug/${FRAMEWORK_NAME}PackageTests.xctest -instr-profile .build/debug/codecov/default.profdata > info.lcov
   bash <(curl https://codecov.io/bash) -F travis -F bionic -n $TRAVIS_JOB_NUMBER-$TRAVIS_OS_NAME
 fi
 
