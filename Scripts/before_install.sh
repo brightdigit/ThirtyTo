5f5bb742d85cfdb211553f207098da6551d2b4d4
leogdion <leogdion@brightdigit.com>
2020-06-02 13:54:13 -0400

    fix arm64 focal

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -6,10 +6,7 @@ if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
 elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
   RELEASE_DOT=$(lsb_release -sr)
   RELEASE_NUM=${RELEASE_DOT//[-._]/}
-  if [[ $TRAVIS_CPU_ARCH == "arm64" ]]; then
-    curl -s https://packagecloud.io/install/repositories/swift-arm/release/script.deb.sh | sudo bash
-    sudo apt-get install swift5
-  else 
+  
   if [[ $RELEASE_DOT == "20.04" ]]; then
     sudo apt-get update
     sudo apt-get -y install \
@@ -29,6 +26,11 @@ elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
             tzdata \
             zlib1g-dev
     fi
+    
+  if [[ $TRAVIS_CPU_ARCH == "arm64" ]]; then
+    curl -s https://packagecloud.io/install/repositories/swift-arm/release/script.deb.sh | sudo bash
+    sudo apt-get install swift5
+  else 
     wget https://swift.org/builds/swift-${SWIFT_VER}-release/ubuntu${RELEASE_NUM}/swift-${SWIFT_VER}-RELEASE/swift-${SWIFT_VER}-RELEASE-ubuntu${RELEASE_DOT}.tar.gz
     tar xzf swift-${SWIFT_VER}-RELEASE-ubuntu${RELEASE_DOT}.tar.gz
   fi 
